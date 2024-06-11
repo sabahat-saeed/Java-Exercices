615d3812f461d1fa5d0b6949603650c9fdfe4231
sabahat-saeed <sabahatsaeed31@gmail.com>
2024-06-11 03:00:13 +0200

    exercise 14

--- src/NumberOfDaysInMonth.java
+++ src/NumberOfDaysInMonth.java
@@ -75,7 +75,7 @@
         };
     }
     public static boolean isLeapYear(int year){
-        if(year < 0 || year > 9999){
+        if(year < 1 || year > 9999){
             return false;
         }
         if(year % 4 == 0){
